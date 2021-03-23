bef7534827b6fbe305ceb5ba0193dcda1d6771a4
mehrozezahid <mehroze.zahid@gmail.com>
2021-03-23 02:50:33 +0100

    Added toastr and basic fixes

--- src/app/containers/default-layout/default-layout.component.ts
+++ src/app/containers/default-layout/default-layout.component.ts
@@ -1,15 +1,19 @@
-import { Component } from "@angular/core";
-import { navItems } from "../../_nav";
+import { Component } from '@angular/core';
+import { navItems } from '../../_nav';
+import * as toastr from 'toastr';
 
 @Component({
-  selector: "app-dashboard",
-  templateUrl: "./default-layout.component.html",
-  styleUrls: ["./default-layout.component.scss"],
+  selector: 'app-dashboard',
+  templateUrl: './default-layout.component.html',
+  styleUrls: ['./default-layout.component.scss'],
 })
 export class DefaultLayoutComponent {
   public sidebarMinimized = false;
   public navItems = navItems;
   public selectedTab = TABS.OVERVIEW;
+
+  constructor() {
+  }
 
   toggleMinimize(e) {
     this.sidebarMinimized = e;
@@ -21,11 +25,19 @@ export class DefaultLayoutComponent {
 
   selectTab(tab: TABS) {
     this.selectedTab = tab;
+    if (tab === TABS.SETTINGS) {
+      this.showToastr();
+    }
+  }
+
+  showToastr() {
+    toastr.warning('This feature is under development. Check back soon!', 'Sorry!', {timeOut: 5000,
+      positionClass: 'toast-top-right', closeButton: true, showMethod: 'show', hideMethod: 'hide'});
   }
 }
 
 export enum TABS {
-  OVERVIEW = "overview",
-  CHALLENGES = "challenges",
-  SETTINGS = "settings",
+  OVERVIEW = 'overview',
+  CHALLENGES = 'challenges',
+  SETTINGS = 'settings',
 }
